b451babe09b76c2b2bc7baa71694cdb83f5cce6b
Jaehyuk-Lee <alex00728@outlook.com>
2020-08-02 20:36:22 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function getBannedMsg() {
+    let str = document.getElementById('beforeBanMsg').value;
+    str = str.replace(/(?:\r\n|\r|\n| |[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000])/g, ',').replace(/,.{1},/,',').replace(/,+/g, ',');
+    document.getElementById('afterBanMsg').value = str;
+}
